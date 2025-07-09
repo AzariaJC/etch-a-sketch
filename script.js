3a89a3da6243c4072156b84677fc4e8234bd6334
AzariaJC <45110670+AzariaJC@users.noreply.github.com>
2025-07-09 09:50:32 -0400

    16 square boxes but not in a grid

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#container");
+const numberOfSquares = 16;
+
+for (let i = 0; i <= numberOfSquares; i++) {
+    const squareDiv = document.createElement('div');
+    squareDiv.classList.add('square');
+    container.appendChild(squareDiv)
+}
