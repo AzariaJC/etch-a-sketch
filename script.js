f36fdd11215227c3ccaf4fedd5a4f0fc0cfc68f0
AzariaJC <45110670+AzariaJC@users.noreply.github.com>
2025-07-09 13:10:42 -0400

    event listeners for mouseover and mouseout nonfunctional

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const container = document.querySelector("#container");
 const numberOfSquares = 256;
 
+
+
 for (let i = 0; i < numberOfSquares; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
     container.appendChild(squareDiv)
 }
+
+const box = document.querySelector('square');
+
+box.addEventListener("mouseover", () => {
+    box.style.backgroundColor ="black";
+});
+box.addEventListener("mouseout", () => {
+    box.style.backgroundColor = "grey";
+});
