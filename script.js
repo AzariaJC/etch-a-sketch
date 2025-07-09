c0373c1c35e2e50ee1e5c24f592355c60bde0818
AzariaJC <45110670+AzariaJC@users.noreply.github.com>
2025-07-09 11:13:12 -0400

    16x16 grid made, off by one square originally, flex-basis calculated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container");
 const numberOfSquares = 256;
 
-for (let i = 0; i <= numberOfSquares; i++) {
+for (let i = 0; i < numberOfSquares; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
     container.appendChild(squareDiv)
