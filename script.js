b966897cf0d70b420479216cb6755fd622eae2d7
AzariaJC <45110670+AzariaJC@users.noreply.github.com>
2025-07-11 15:54:16 -0400

    changed order of code (functions on top), made numberOfSqaures & container arguments

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
+
 const container = document.querySelector("#container");
-let numberOfSquares = 16;
-
-
-
-function generateSquares () {
-    for (let i = 0; i < numberOfSquares ** 2; i++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.classList.add('square');
-    container.appendChild(squareDiv)
-    squareDiv.style.backgroundColor = 'green';
+function generateSquares(numberOfSquares, container) {
+  for (let i = 0; i < numberOfSquares ** 2; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    container.appendChild(squareDiv);
+    squareDiv.style.backgroundColor = "green";
     squareDiv.style.border = "2px solid black";
     squareDiv.style.display = "flex";
-    squareDiv.style.flex = '1 1 calc((100/numberOfSquares) - 8px)';
+    squareDiv.style.flex = `1 1 calc((${100/numberOfSquares})% - 8px)`;
     squareDiv.style.height = "50px";
+    squareDiv.addEventListener("mouseover", changeBackground);
+  }
 }
-};
 
-const boxes = document.querySelectorAll('.square');
+function changeGridSize(numberOfSquares, container) {
+  let newGridSizeInput = prompt(
+    "Please input how many rows of squares you would like:",
+    "ex: 10 will give you a 10 x 10 grid"
+  );
+  if (newGridSizeInput > 100) {
+    alert("That number is too high! Pick a number 100 or less.");
+  } else {
+    numberOfSquares = newGridSizeInput;
+    container.replaceChildren();
+    generateSquares();
+  }
+}
 
 function changeBackground(e) {
-    e.currentTarget.style.backgroundColor = "black";
-};
+  e.currentTarget.style.backgroundColor = "black";
+}
 
-boxes.forEach(function(box){
-    box.addEventListener("mouseover", changeBackground);
-});
-
+// ABOVE THIS LINE ARE ALL FUNCTIONS USED
 const topButton = document.querySelector("#changeGrid");
 
 topButton.addEventListener("click", changeGridSize);
 
-function changeGridSize() {
-    let newGridSizeInput = prompt("Please input how many rows of squares you would like:", "ex: 10 will give you a 10 x 10 grid");
-    if (newGridSizeInput > 100) {
-        alert("That number is too high! Pick a number 100 or less.");
-    } else {
-        numberOfSquares = newGridSizeInput;
-        container.replaceChildren();
-        generateSquares();
-    }
-};
-
-generateSquares();
+generateSquares(16, container);
